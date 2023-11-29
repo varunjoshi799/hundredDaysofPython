76c8a8ec97f5234711eea3c96a450cade74baca4
Varun Joshi <varunjoshi@Varuns-MacBook-Pro.local>
2023-11-29 11:39:53 -0800

    day 1

---
+++ dayOne/bandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your first pet?\n")
+print("Your band name is: " + city + " " + pet)
