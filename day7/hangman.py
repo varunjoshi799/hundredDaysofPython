fbc3ffa14a4f19407114f8d48c25143887a0fea2
Varun Joshi <varunjoshi@Varuns-MacBook-Pro.local>
2023-12-06 08:48:07 -0800

    day 7 continued

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,21 +1,37 @@
 import random
+from hangman_words import word_list
+from hangman_art import stages, logo
 
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
 word_array = ['_'] * len(chosen_word)
 gameEnd = False
+lives = 6
+
+print(logo)
+print("Welcome to hangman!")
 
 while not gameEnd:
-    print(f'Pssst, the solution is {chosen_word}.')
+    print("\n---------------------\n")
     guess = input("Guess a letter: ").lower()
 
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"'{guess}' is not in the chosen word. You lost a life. Try again.")
+        if lives == 0:
+            print("\nYou lose.")
+            print(f"The word was {chosen_word}")
+            gameEnd = True
+            break
+    
+    if guess in word_array:
+        print(f"\nYou already guessed the letter '{guess}'. Try again.")
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             word_array[i] = guess
 
-    print(word_array)
+    print(stages[lives])
+    print(f"{' '.join(word_array)}")
     if "_" not in word_array:
         gameEnd = True
+        print("\nYou win.")
 
-print("You win.")
